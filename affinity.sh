b5634b9b69ccd8afefef37cc23317cc8bba3c488
Jade Lovelace <software@lfcode.ca>
2024-01-23 19:12:35 -0800

    setup.sh should definitely work now?

--- affinity.sh
+++ affinity.sh
@@ -2,13 +2,14 @@
 
 selfpath=$(realpath "$(dirname "$0")")
 winepath=$(readlink "$selfpath/wine")
+(return 0 2>/dev/null) && sourced=1 || sourced=0
 
 export WINEPREFIX="${WINEPREFIX:-$HOME/.local/share/wineAffinity}"
 export PATH="$winepath/bin:$PATH"
-export LD_LIBRARY_PATH="$winepath/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$winepath/lib${LD_LIBRARY_PATH:+:}${LD_LIBRARY_PATH:-}"
 export WINEDLLOVERRIDES="winemenubuilder.exe=d"
 export WINESERVER="$winepath/bin/wineserver"
 export WINELOADER="$winepath/bin/wine"
 export WINEDLLPATH="$winepath/lib/wine"
 
-"$@"
+if [[ $sourced == 0 ]]; then exec "$@"; fi
--- setup.sh
+++ setup.sh
@@ -5,10 +5,13 @@ set -eu
 selfpath=$(realpath "$(dirname "$0")")
 wineEnv="$selfpath/wine"
 
-./affinity.sh wineboot --init
+source "$selfpath/affinity.sh"
+
+# this crime is required to make wineboot not try to install mono itself
+WINEDLLOVERRIDES="mscoree=" wineboot --init
 # FIXME: fix in wine packaging
-./affinity.sh wine "$wineEnv/share/wine/mono/wine-mono-8.1.0-x86.msi"
-./affinity.sh winetricks -q dotnet48 corefonts
-./affinity.sh wine winecfg -v win11
+wine msiexec /i "$wineEnv/share/wine/mono/wine-mono-8.1.0-x86.msi"
+winetricks -q dotnet48 corefonts
+wine winecfg -v win11
 # grab from a real windows computer
 cp -r "$selfpath/license_violations/WinMetadata" "$WINEPREFIX/drive_c/windows/system32/"
